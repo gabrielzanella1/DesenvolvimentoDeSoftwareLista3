45e6a6b75ca051ae70f8ebf90ddd89e953d52759
Gabriel <gzanella202@gmail.com>
2024-03-28 11:10:44 -0300

    Exercicio14 e Classe Contabilidade

---
+++ Contabilidade.java
@@ -0,0 +1,27 @@
+public class Contabilidade {
+    private double precoCusto;
+    private double precoVenda;
+
+    public Contabilidade(double precoCusto, double precoVenda) {
+        this.precoCusto = precoCusto;
+        this.precoVenda = precoVenda;
+    }
+
+    public String calcularResultado() {
+        if (precoVenda > precoCusto) {
+            return "Lucro";
+        } else if (precoVenda < precoCusto) {
+            return "Prejuízo";
+        } else {
+            return "Empate";
+        }
+    }
+
+    public double getPrecoCusto() {
+        return precoCusto;
+    }
+
+    public double getPrecoVenda() {
+        return precoVenda;
+    }
+}
--- Exercicio14.java
+++ Exercicio14.java
@@ -1,33 +1,24 @@
 public class Exercicio14 {
     public static void executar(){
-        double totalCusto = 0;
-        double totalVenda = 0;
+        double totalPrecoCusto = 0;
+        double totalPrecoVenda = 0;
 
         for (int i = 0; i < 40; i++) {
-            double custo = Prompt.lerDecimal("Digite o preço de custo do produto " + (i + 1) + ": ");
-            double venda = Prompt.lerDecimal("Digite o preço de venda do produto " + (i + 1) + ": ");
+            double precoCusto = Prompt.lerDecimal("Informe o preço de custo do produto " + (i + 1) + ": ");
+            double precoVenda = Prompt.lerDecimal("Informe o preço de venda do produto " + (i + 1) + ": ");
 
-            double fechamento = venda - custo;
-            String resultado;
-            if (fechamento > 0) {
-                resultado = "Lucro";
-            } else if (fechamento < 0) {
-                resultado = "Prejuízo";
-            } else {
-                resultado = "Deu elas por elas.";
-            }
+            Contabilidade produto = new Contabilidade(precoCusto, precoVenda);
+            String resultado = produto.calcularResultado();
+            Prompt.imprimir("Produto " + (i + 1) + ": " + resultado);
 
-            Prompt.imprimir("Resultado para o produto " + (i + 1) + ": " + resultado);
-
-            totalCusto += custo;
-            totalVenda += venda;
+            totalPrecoCusto += precoCusto;
+            totalPrecoVenda += precoVenda;
         }
 
-        double mediaCusto = totalCusto / 40;
-        double mediaVenda = totalVenda / 40;
+        double mediaPrecoCusto = totalPrecoCusto / 40;
+        double mediaPrecoVenda = totalPrecoVenda / 40;
 
-        Prompt.linhaEmBranco();
-        Prompt.imprimir("Média de preço de custo: " + mediaCusto);
-        Prompt.imprimir("Média de preço de venda: " + mediaVenda);
+        Prompt.imprimir("Média de preço de custo: R$ " + mediaPrecoCusto);
+        Prompt.imprimir("Média de preço de venda: R$ " + mediaPrecoVenda);
     }
 }
