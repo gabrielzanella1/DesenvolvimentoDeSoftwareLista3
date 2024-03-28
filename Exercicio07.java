40cfbd98e4aed5da2e1bb7d539d9ad49ed5cf740
Gabriel <gzanella202@gmail.com>
2024-03-28 10:47:35 -0300

    Exercicio07 e Classe Carro

--- Exercicio07.java
+++ Exercicio07.java
@@ -1,14 +1,11 @@
 public class Exercicio07 {
     public static void executar(){
-        double custoFabrica = Prompt.lerDecimal("Digite o custo de fábrica do carro: ");
+        double custoFabrica = Prompt.lerDecimal("Informe o custo de fábrica do carro: ");
 
-        double imposto = 0.45;
-        double distribuidor = 0.28;
+        Carro carro = new Carro(custoFabrica);
+        double custoConsumidor = carro.calcularCustoConsumidor();
 
-        double impostos = custoFabrica * imposto;
-        double custoImpostos = custoFabrica + impostos;
-        double custoConsumidor = custoImpostos + (custoImpostos * distribuidor);
-
-        Prompt.imprimir("O custo ao consumidor do carro é: " + custoConsumidor);
+        Prompt.imprimir("O custo ao consumidor do carro é: R$ " + custoConsumidor);
     }
 }
+
