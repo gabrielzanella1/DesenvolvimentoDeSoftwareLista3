7229259ac01282bea55102a7a1c2a85ce7304fdb
Gabriel <gzanella202@gmail.com>
2024-03-28 10:39:19 -0300

    Exercicio06 e Classe Produto

--- Exercicio06.java
+++ Exercicio06.java
@@ -1,10 +1,11 @@
 public class Exercicio06 {
     public static void executar() {
-        double custo = Prompt.lerDecimal("Digite o preço de custo do produto: ");
-        double acrescimo = Prompt.lerDecimal("Digite o percentual de acréscimo: ");
+        double precoCusto = Prompt.lerDecimal("Informe o preço de custo do produto: ");
+        double percentualAcrescimo = Prompt.lerDecimal("Informe o percentual de acréscimo (%): ");
 
-        double venda = custo * (1 + acrescimo / 100);
+        Produto produto = new Produto(precoCusto);
+        double precoVenda = produto.calcularPrecoVenda(percentualAcrescimo);
 
-        Prompt.imprimir("O preço de venda do produto é: " + venda);
+        Prompt.imprimir("O preço de venda do produto é: R$ " + precoVenda);
     }
 }
---
+++ Produto.java
@@ -0,0 +1,11 @@
+public class Produto {
+    private double precoCusto;
+
+    public Produto(double precoCusto) {
+        this.precoCusto = precoCusto;
+    }
+
+    public double calcularPrecoVenda(double percentualAcrescimo) {
+        return precoCusto * (1 + percentualAcrescimo / 100);
+    }
+}
