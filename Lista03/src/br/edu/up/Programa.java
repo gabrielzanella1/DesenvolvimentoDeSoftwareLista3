70813f0f79af4a2deb07f8fb28e236766d958875
gabriel <gzanella202@gmail.com>
2024-04-19 20:11:44 -0300

    Exercicio15

--- Lista03/src/br/edu/up/Programa.java
+++ Lista03/src/br/edu/up/Programa.java
@@ -4,47 +4,47 @@ import br.edu.up.exercicios.*;
 
 public class Programa {
     public static void main(String[] args) throws Exception {
-        // Exercicio01 exercicio1 = new Exercicio01();
-        // exercicio1.Executar();
+        Exercicio01 exercicio1 = new Exercicio01();
+        exercicio1.Executar();
 
-        // Exercicio02 exercicio02 = new Exercicio02();
-        // exercicio02.Executar();
+        Exercicio02 exercicio02 = new Exercicio02();
+        exercicio02.Executar();
 
-        // Exercicio03 exercicio03 = new Exercicio03();
-        // exercicio03.Executar();
+        Exercicio03 exercicio03 = new Exercicio03();
+        exercicio03.Executar();
 
-        // Exercicio04 exercicio04 = new Exercicio04();
-        // exercicio04.Executar();
+        Exercicio04 exercicio04 = new Exercicio04();
+        exercicio04.Executar();
 
-        // Exercicio05 exercicio05 = new Exercicio05();
-        // exercicio05.Executar();
+        Exercicio05 exercicio05 = new Exercicio05();
+        exercicio05.Executar();
 
-        // Exercicio06 exercicio06 = new Exercicio06();
-        // exercicio06.Executar();
+        Exercicio06 exercicio06 = new Exercicio06();
+        exercicio06.Executar();
 
-        // Exercicio07 exercicio07 = new Exercicio07();
-        // exercicio07.Executar();
+        Exercicio07 exercicio07 = new Exercicio07();
+        exercicio07.Executar();
 
-        // Exercicio08 exercicio08 = new Exercicio08();
-        // exercicio08.Executar();
+        Exercicio08 exercicio08 = new Exercicio08();
+        exercicio08.Executar();
 
-        // Exercicio09 exercicio09 = new Exercicio09();
-        // exercicio09.Executar();
+        Exercicio09 exercicio09 = new Exercicio09();
+        exercicio09.Executar();
 
-        // Exercicio10 exercicio10 = new Exercicio10();
-        // exercicio10.Executar();
+        Exercicio10 exercicio10 = new Exercicio10();
+        exercicio10.Executar();
 
-        // Exercicio11 exercicio11 = new Exercicio11();
-        // exercicio11.Executar();
+        Exercicio11 exercicio11 = new Exercicio11();
+        exercicio11.Executar();
 
-        // Exercicio12 exercicio12 = new Exercicio12();
-        // exercicio12.Executar();
+        Exercicio12 exercicio12 = new Exercicio12();
+        exercicio12.Executar();
 
-        // Exercicio13 exercicio13 = new Exercicio13();
-        // exercicio13.Executar();
+        Exercicio13 exercicio13 = new Exercicio13();
+        exercicio13.Executar();
 
-        // Exercicio14 exercicio14 = new Exercicio14();
-        // exercicio14.Executar();
+        Exercicio14 exercicio14 = new Exercicio14();
+        exercicio14.Executar();
 
         Exercicio15 exercicio15 = new Exercicio15();
         exercicio15.Executar();
